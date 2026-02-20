083560b8b70e2fd187a3861d77cf577b291a5ecf
Kankawee Seetalarom <132635060+Meepython@users.noreply.github.com>
2026-02-20 18:01:20 +0700

    test

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,27 @@
-export class User {}
+export class User {
+    firstName: string = ''
+    lastName: string = ''
+    age: number = 0
+
+    static BIRTH_YEAR: number = 2000;
+
+    setFirstname(name: string):void {
+        this.firstName = name
+    }
+
+    setLastname(name: string):void {
+        this.lastName = name
+    }
+
+    setAge(age:number):void {
+        this.age = age
+    }
+
+    getFullName(): string {
+        return `${this.firstName} ${this.lastName}`
+    }
+
+    getAge(): number {
+        return this.age
+    }
+}

63eb4a9e2c14a2f7200424a970863842446a47dd
Kankawee Seetalarom <132635060+Meepython@users.noreply.github.com>
2026-02-20 17:38:41 +0700

    rwararw

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,4 +1,20 @@
 export class Car {
     public brand:string = '';
     public model:string = '';
+
+    start() {
+        console.log(`${this.brand} ${this.model} is starting...`)
+    }
+
+    accelerate() {
+        console.log(`${this.brand} ${this.model} is accelerating...`)
+    }
+
+    brake() {
+        console.log(`${this.brand} ${this.model} is braking...`)
+    }
+
+    stop() {
+        console.log(`${this.brand} ${this.model} is stopping...`)
+    }
 }
